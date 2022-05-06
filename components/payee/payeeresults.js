c8d0bfa8e60afcd37909ab691b3a2032dccc25f2
joshy24 <joshuamajebi@yahoo.com>
2022-05-06 12:32:27 +0100

    fixed admin search issue

--- components/payee/payeeresults.js
+++ components/payee/payeeresults.js
@@ -8,7 +8,9 @@ const closeBtnContent = {
     justifyContent: "flex-end", 
     padding: "8px 16px 8px 16px", 
     position: "fixed",
-    background: "url(0,0,0,0)"
+    background: "url(0,0,0,0)",
+    position: "fixed",
+    top: "0"
 }
 
 const PayeeResults = ({members, setMember, setMembers}) => {
